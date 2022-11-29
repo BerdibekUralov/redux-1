8f0a56e094147e8e98eb63b69fc7a29452c1389f
BerdibekUralov <uralov0212@gmail.com>
2022-11-29 12:20:25 +0300

    2-урок

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore } from 'redux'
 
-const initailState = 0
+const initailState = {value: 0}
 
 const reducer = (state = initailState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return state += 1  
+      return {
+        ...state,
+        value: state.value + 1
+      }
     case "DECREMENT":
-      return state -= 1  
+      return {
+        ...state,
+        value: state.value - 1
+      }
+    case "RANDOM":
+      return {
+        ...state,
+        value: state.value * action.payload
+      }
     default:
       return state;
   }
@@ -17,17 +28,31 @@ const reducer = (state = initailState, action) => {
 
 const store = createStore(reducer)
 
-store.subscribe(() => console.log(store.getState()))
+const update = () => document.getElementById('counter').textContent = store.getState().value
+store.subscribe(update)
 
-store.dispatch({ type: 'INCREMENT' })
-store.dispatch({ type: 'INCREMENT' })
-store.dispatch({ type: 'DECREMENT' })
+const dec = () => ({type: 'DECREMENT'})
+const inc = () => ({type: 'INCREMENT'})
+const random = (num) => ({type: 'RANDOM', payload: num})
+
+document.getElementById('increment').addEventListener('click', () => {
+  store.dispatch(inc())
+})
+document.getElementById('decrement').addEventListener('click', () => {
+  store.dispatch(dec())
+})
+document.getElementById('random').addEventListener('click', () => {
+  const randomValue = Math.floor(Math.random() * 10)
+  store.dispatch(random(randomValue))
+})
+
+document.getElementById('counter').textContent()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
-      Hello
+      {/* <h3 style={{textAlign: 'center'}}>Hello Redux </h3> */}
     </>
   </React.StrictMode>
 );
